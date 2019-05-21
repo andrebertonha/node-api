0127b9950d25f686f658562984e3591af9fd34ec
André Martins <andrebertonhadev@gmail.com>
2019-05-21 18:32:36 -0300

    node js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+//Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors()); //parametros dominios que podem acessar a api e/ou configurações a mais de segurança
+
+//Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', 
+{ useNewUrlParser: true}
+); //andrebertonha@senha
+
+requireDir('./src/models');
+
+
+//Rotas
+app.use('/api', require('./src/routes'))
+
+app.listen(3001);
